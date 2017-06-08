40e4827280aea05ebb02575284ecd4a4ddcf2878
Jennifer Gilbert <j.h.gilbert@gmail.com>
2017-06-07 22:23:57 -0700

    Stub in card charger method and test it

--- store.rb
+++ store.rb
@@ -26,7 +26,28 @@ def stringify_to_dollars(cents)
   "$#{dollars}"
 end
 
+def run_charge(user_card, amount)
+  puts "-" * 10    # this just prints "----------"
+  puts
+  puts "The charge method would run here,"
+  puts "but there's not much logic here yet."
+  puts "You'll add that later!"
+  puts "It would use this card info:"
+  puts "Card number: #{user_card[:number]}"
+  puts "Card exp: #{user_card[:exp_month]}/#{user_card[:exp_year]}"
+  puts "Card cvc: #{user_card[:cvc]}"
+  puts
+  puts "And it would use this amount:"
+  puts
+  p amount
+  puts "-" * 10
+  # TODO: Create a Stripe token
+  # TODO: Use the token to create a Stripe charge
+  puts "Your card has been charged #{stringify_to_dollars(amount)}."
+end
+
 # driver code
 
 card = get_card_info
 p stringify_to_dollars(250)
+run_charge(card, 250)
