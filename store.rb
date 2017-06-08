7f9c5e6609bd3b5d5e6413981847600807053975
Jennifer Gilbert <j.h.gilbert@gmail.com>
2017-06-07 22:22:19 -0700

    Get user's card info

--- store.rb
+++ store.rb
@@ -2,3 +2,22 @@ require 'stripe'
 require_relative 'stripe_config'
 
 Stripe.api_key = $STRIPE_SECRET_TEST_KEY
+
+# method definitions
+
+def get_card_info
+  card = {}
+  puts "Enter your credit card number:"
+  card[:number] = gets.chomp
+  puts "Enter your expiration month:"
+  card[:exp_month] = gets.chomp
+  puts "Enter your expiration year:"
+  card[:exp_year] = gets.chomp
+  puts "Enter your CVC:"
+  card[:cvc] = gets.chomp
+  card
+end
+
+# driver code
+
+card = get_card_info
